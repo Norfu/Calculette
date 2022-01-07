8ce816ff2ae2382e4e190a72efa3083d53714628
Quentin DESTEF <quentin.destefanis@gmail.com>
2022-01-07 14:49:46 +0100

    final commit

--- Calculatrice.js
+++ Calculatrice.js
@@ -1,90 +1,105 @@
-function functionAdd(value){
-    if(document.getElementById("operateur").innerHTML == false){
-        document.getElementById("nombreX").innerHTML += value;}
-    else{
-        document.getElementById("nombreY").innerHTML += value;
+function functionAdd(value) {
+    if (document.getElementById("operateur").innerHTML == false) {
+        if(document.getElementById("nombreX").innerHTML.length < 8){
+        document.getElementById("nombreX").innerHTML += value;
+        }
     }
-    document.getElementById("resultat").innerHTML = '';
-}
-
-
-
-function functionOperateur(value){
-    document.getElementById("operateur").innerHTML = value;
+    else {
+        if(document.getElementById("nombreY").innerHTML.length < 8){
+        document.getElementById("nombreY").innerHTML += value;
+        }
+    }
     
 }
 
-function functionResult(){
-    let x = document.getElementById("nombreX").innerHTML ;
-    let y = document.getElementById("nombreY").innerHTML ;
 
-    if (document.getElementById("operateur").innerHTML == '+'){
-        document.getElementById("nombreX").innerHTML = null ;
-        document.getElementById("nombreY").innerHTML = null ;
-        document.getElementById("operateur").innerHTML = null ;
-        document.getElementById("resultat").innerHTML = ((+x)+(+y));
-    }
-    if (document.getElementById("operateur").innerHTML == '-'){
-        document.getElementById("nombreX").innerHTML = null ;
-        document.getElementById("nombreY").innerHTML = null ;
-        document.getElementById("operateur").innerHTML = null ;
-        document.getElementById("resultat").innerHTML = ((+x)-(+y));
-    }
-    if (document.getElementById("operateur").innerHTML == '*'){
-        document.getElementById("nombreX").innerHTML = null ;
-        document.getElementById("nombreY").innerHTML = null ;
-        document.getElementById("operateur").innerHTML = null ;
-        document.getElementById("resultat").innerHTML = ((+x)*(+y));
-    }
-    if (document.getElementById("operateur").innerHTML == '/'){
-        document.getElementById("nombreX").innerHTML = null ;
-        document.getElementById("nombreY").innerHTML = null ;
-        document.getElementById("operateur").innerHTML = null ;
-        document.getElementById("resultat").innerHTML = ((+x)/(+y));
-    }
-}
 
-function functionCE(){
-    document.getElementById("nombreX").innerHTML = null ;
-    document.getElementById("nombreY").innerHTML = null ;
-    document.getElementById("operateur").innerHTML = null ;
-    document.getElementById("resultat").innerHTML = null;
+function functionOperateur(value) {
+    document.getElementById("operateur").innerHTML = value;
 
 }
 
-function functionC(){
-    if (document.getElementById("nombreY").innerHTML != ''){
+function functionResult() {
+    let x = document.getElementById("nombreX").innerHTML;
+    let y = document.getElementById("nombreY").innerHTML;
+
+    if (document.getElementById("operateur").innerHTML == '+') {
+        
+        document.getElementById("nombreX").innerHTML = ((+x) + (+y));
+    }
+    if (document.getElementById("operateur").innerHTML == '-') {
+        document.getElementById("nombreX").innerHTML = ((+x) - (+y));
+    }
+    if (document.getElementById("operateur").innerHTML == '*') {
+        document.getElementById("nombreX").innerHTML = ((+x) * (+y));
+    }
+    if (document.getElementById("operateur").innerHTML == '/') {
+        document.getElementById("nombreX").innerHTML = ((+x) / (+y));
+    }
+    document.getElementById("nombreY").innerHTML = '';
+    document.getElementById("operateur").innerHTML ='';
+    
+}
+
+function functionCE() {
+    document.getElementById("nombreX").innerHTML = '';
+    document.getElementById("nombreY").innerHTML = '';
+    document.getElementById("operateur").innerHTML = '';
+    
+
+}
+
+function functionC() {
+    if (document.getElementById("nombreY").innerHTML != '') {
         document.getElementById("nombreY").innerHTML = '';
-    }else if(document.getElementById("operateur").innerHTML != ''){
+    } else if (document.getElementById("operateur").innerHTML != '') {
         document.getElementById("operateur").innerHTML = '';
-    }else if (document.getElementById("nombreX").innerHTML != ''){
+    } else if (document.getElementById("nombreX").innerHTML != '') {
         document.getElementById("nombreX").innerHTML = '';
     }
     document.getElementById("resultat").innerHTML = '';
 }
+
+function functionDelete() {
+    if (document.getElementById("nombreY").innerHTML != '') {
+        document.getElementById("nombreY").innerHTML = document.getElementById("nombreY").innerHTML.slice(0, -1);
+    } else if (document.getElementById("operateur").innerHTML != '') {
+        document.getElementById("operateur").innerHTML = document.getElementById("operateur").innerHTML.slice(0, -1);
+    } else if (document.getElementById("nombreX").innerHTML != '') {
+        document.getElementById("nombreX").innerHTML = document.getElementById("nombreX").innerHTML.slice(0, -1);
+    }
+}
+
+function functionNegativ() {
+    if (document.getElementById("nombreY").innerHTML != '') {
+        document.getElementById("nombreY").innerHTML = parseInt(document.getElementById("nombreY").innerHTML, 10) * -1
+    } else if (document.getElementById("nombreX").innerHTML != '') {
+        document.getElementById("nombreX").innerHTML = parseInt(document.getElementById("nombreX").innerHTML, 10) * -1
+    }
+}
+
+function functionComa() {
     
-function functionDelete(){
-    if (document.getElementById("nombreY").innerHTML != ''){
-        document.getElementById("nombreY").innerHTML=document.getElementById("nombreY").innerHTML.slice(0,-1);
-    }else if(document.getElementById("operateur").innerHTML != ''){
-        document.getElementById("operateur").innerHTML=document.getElementById("operateur").innerHTML.slice(0,-1);
-    }else if (document.getElementById("nombreX").innerHTML != ''){
-        document.getElementById("nombreX").innerHTML = document.getElementById("nombreX").innerHTML.slice(0,-1);
+    var virgule = false;
+    
+    if (document.getElementById("operateur").innerHTML == ''){
+        for (i=0;i<document.getElementById("nombreX").innerHTML.length;i++){
+            if (document.getElementById("nombreX").innerHTML[i] == '.'){
+                virgule=true;
+            }
+        }
+        if (virgule==false) {
+            document.getElementById("nombreX").innerHTML += '.';
+        }
+    }else{
+        for (i=0;i<document.getElementById("nombreY").innerHTML.length;i++){
+            if (document.getElementById("nombreY").innerHTML[i] == '.'){
+                virgule=true;
+            }
+        }
+        if (virgule==false) {
+            document.getElementById("nombreY").innerHTML += '.';
+        }
     }
+    
 }
-
-function functionNegativ(){
-    if (document.getElementById("nombreY").innerHTML !=''){
-        document.getElementById("nombreY").innerHTML = parseInt(document.getElementById("nombreY").innerHTML,10)* -1
-    }else if (document.getElementById("nombreX").innerHTML != ''){
-        document.getElementById("nombreX").innerHTML = parseInt(document.getElementById("nombreX").innerHTML,10)* -1
-    }
-}
-
-// function functionComa(){
-//     if (document.getElementById("nombreY").innerHTML !=''){
-//         document.getElementById("nombreY").innerHTML = parseInt(document.getElementById("nombreY").innerHTML,10)*1
-//     }else if (document.getElementById("nombreX").innerHTML != ''){
-//         document.getElementById("nombreX").innerHTML = parseInt(document.getElementById("nombreX").innerHTML,10)* -1
-//     }
-// }
